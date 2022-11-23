036827576ac3dfc4e6d0d4fadf0c1f125213e6d7
Gabriel Kryger <gabriel.kryger@fing.edu.uy>
2022-11-22 22:54:39 -0300

    loadPeliculasDist

---
+++ GeneradorDeEstructuras/LoadPeliculasDistibuidores.py
@@ -0,0 +1,70 @@
+#inport csv to database sqlite3 
+#the csv file is in ../data/titles.csv
+#the database is in dataBase/Peliculas.db
+#the table is in PeliculasDistibuidores
+#the table has 3 columns: title, distributors
+#the csv has 4 columns: Id;title;Distributor;type
+#uses the id from the table Peliculas
+#the table Peliculas has 2 columns: id, titulo
+
+import csv
+import sqlite3
+
+#connect to database
+conn = sqlite3.connect('./dataBase/Peliculas.db')
+c = conn.cursor()
+
+#create table
+
+c.execute('''DROP TABLE IF EXISTS PeliculasDistibuidores''')
+c.execute('''CREATE TABLE PeliculasDistibuidores
+                (title text, distributors text)''')
+
+#open csv file
+with open('../data/titles.csv', 'r', encoding="utf8") as f:
+    #reeds the csv with the delimiter ;
+    reader = csv.reader(f, delimiter=';')
+    #skip the first line
+    next(reader)
+    #insert the data in the table
+    count = 0
+    for row in reader:
+        count += 1
+        if count % 1000 == 0:
+            print(count)
+            
+        #gets the existing distruvibutors
+        c.execute("SELECT distributors FROM PeliculasDistibuidores WHERE title = ?", (row[1],))
+        distributors = c.fetchone()
+        #gets the id from the table Peliculas
+        c.execute("SELECT id FROM Peliculas WHERE title = ?", (row[1],))
+        idPelicula = c.fetchone()
+        
+        #if the title is not in the table Peliculas
+        if idPelicula is None:
+            print("title not found: " + str(row[1]) + " " + str(row[2]) + str(count))
+            continue
+
+        #sets row[1] to the id from the table Peliculas
+        row[1] = idPelicula[0]
+
+        #if the movie is not in the table
+        if distributors is None:
+            #insert the movie and the distributor
+            c.execute("INSERT INTO PeliculasDistibuidores VALUES (?, ?)", (row[1], row[2]))
+        
+        #if the movie is in the table
+        else:
+            #cheks if the distributor is already in the table
+            if distributors[0].find(row[2]) == -1:
+                #splits the distributors
+                distributorsAux = distributors[0].split(',')
+                distributorsAux.append(row[2])
+                distributorsAux.sort()
+                distributors = ','.join(distributorsAux)
+                #insert the movie and the distributor
+                c.execute("UPDATE PeliculasDistibuidores SET distributors = ? WHERE title = ?", (distributors, row[1]))
+        conn.commit()
+
+#close the connection
+conn.close()
